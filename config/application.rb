a0327bbfc5b5ae34bff7f2f26aac926fee889c0b
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2019-10-14 22:05:48 +0900

    Apply rails_base template by Kiribori.   * スワップファイルがリポジトリに紛れ込まないよう `.gitignore` に `*.swp` を追加   * 個人のDB接続情報がリポジトリに混入しないよう `config/database.yml` をリポジトリから排除。代替として `config/database.yml.example` を追加   * タイムゾーンと、デフォルトの辞書を日本・東京に準拠するよう変更   * パスワードハッシュ化関数として `bcrypt` を取り込み   * Windowsでの実行は想定しないため、 `tzinfo-data` を除外   * I18n化のため `rails-i18n` を追加し、辞書ファイルを拡充   * `i18n`のフォールバック先を `[I18n.default_locale]` に修正

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ Bundler.require(*Rails.groups)
 
 module KiriboriSample
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
